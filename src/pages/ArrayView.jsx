1ebf73e782dc402dc634c1a750d65d487e68fa03
Vani Pawar <vanipawar@MacBookPro.fios-router.home>
2025-01-03 15:12:11 -0500

    set up the linked list page, and the linked list animation component. The linked list animation component has unfinished animation methods that team will work on

--- src/pages/ArrayView.jsx
+++ src/pages/ArrayView.jsx
@@ -13,14 +13,6 @@ function ArrayView() {
         <h1>Introduction to Arrays</h1>
         <Animation1></Animation1>
       </div><br></br>
-      <div>
-        <h1>Second Array Animation</h1>
-        <Animation2></Animation2>
-      </div><br></br>
-      <div>
-        <h1>Third Array Animation</h1>
-        <Animation3></Animation3>
-      </div>
     </>
   );
 }
