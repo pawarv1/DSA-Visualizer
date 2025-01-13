400275229f0ca446e1e4fb28771825c8c11c44f4
JacobLovesDrumming <118081708+JacobLovesDrumming@users.noreply.github.com>
2025-01-13 12:12:23 -0500

    Stacks & Queues Anim. 2

---
+++ NavBar.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import ArrayView from './pages/ArrayView';
+import LinkedListsView from './pages/LinkedListsView';
+import HashingView from './pages/HashingView';
+import StacksQueuesView from './pages/StacksQueuesView';
+
+/*
+The navbar component handles page routing
+*/
+
+function NavBar() {
+  return (
+    <Router>
+      <header>
+        <nav>
+          <ul className="navbar">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/array">Arrays</Link></li>
+            <li><Link to="/linkedlist">Linked Lists</Link></li>
+            <li><Link to="/hashing">Hashing</Link></li>
+            <li><Link to="/stacksqueues">Stacks & Queues</Link></li>
+          </ul>
+        </nav><br></br><br></br>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/array" element={<ArrayView></ArrayView>} />
+          <Route path="/linkedlist" element={<LinkedListsView></LinkedListsView>} />
+          <Route path="/hashing" element={<HashingView></HashingView>} />
+          <Route path="/stacksqueues" element={<StacksQueuesView></StacksQueuesView>} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default NavBar;
